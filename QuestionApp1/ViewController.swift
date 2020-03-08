33cdde25b1e2a400312eb1ed8ac1c840539080d6
output <output.org@gmail.com>
2020-03-08 16:05:02 +0900

    Initial Commit

---
+++ QuestionApp1/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  QuestionApp1
+//
+//  Created by output. on 2020/03/08.
+//  Copyright © 2020 tomoaki narita. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
